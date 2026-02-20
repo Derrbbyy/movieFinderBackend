f60cff3987a01080c4c9d33e870ed04064843bef
Derrbbyy <onuohdeborah23@gmail.com>
2026-02-20 23:22:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+
+const app = express();
+const PORT = 3000;
+dotenv.config();
+app.use(cors());
+const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+const API_KEY = process.env.API_KEY;
+
+app.get('/api/popular',async (req,res)=>{
+    try{
+
+        const response = await fetch (
+            `${TMDB_BASE_URL}/api/search/?api_key=${API_KEY}}`
+        )
+        const data = await response.json()
+          response(data)
+
+    }catch(error){
+        res.statusCode(500).json({error: 'Error fetching popular movies'})
+    }
+})
+
+app.get('/api/search',async (req,res)=>{
+    const {query,page} = req.query;
+    try{ const response = await fetch(
+        `${TMDB_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`)
+     const data = await response.json();
+    response.json(data);
+    
+    }catch(error){
+         res.status(500).json({ error: "Error fetching movie details" })
+
+    }});
+
+    app.get("/api/movie/:id", async (req, res) => {
+
+        const { id } = req.params;
+        try {
+          const response = await fetch(
+            `${TMDB_BASE_URL}/movie/${id}?api_key=${API_KEY}`
+          );
+          const data = await response.json();
+          res.json(data);
+        } catch (error) {
+          res.status(500).json({ error: "Error fetching movie details" });
+        }
+    })
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
